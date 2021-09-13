b933e18530c4ced5a19d741f3c9fe889c795e801
zameer fouzan <zameer.fouzan@mitel.com>
2021-09-13 19:28:12 +0530

    added basic auth guard

--- src/app/modules/sample/sample-routing.module.ts
+++ src/app/modules/sample/sample-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@core/services/auth-guard.guard';
 import { SampleComponent } from './components/sample.component';
 
-const routes: Routes = [{ path: '', component: SampleComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: SampleComponent,
+    canActivate:[AuthGuard]
+   }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
